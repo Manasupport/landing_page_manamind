106f2445833a52a76ba34caac265a994314f376a
manayanis <yanis@manadvise.fr>
2024-12-25 15:08:14 +0000

    fdfd

--- src/components/ConsultationSection.tsx
+++ src/components/ConsultationSection.tsx
@@ -23,7 +23,7 @@ export const ConsultationSection = () => {
           </h2>
           <p className="text-lg text-slate-600 leading-relaxed">
             Hello, c'est Yanis, Growth Hacker chez{" "}
-            <span className="text-[#71c088] font-semibold">Mana</span>. Discutons pour répondre à vos questions, explorer vos besoins, t vous aider à choisir l'offre idéale. Consultez moi, j’en serais ravi, c’est rapide, gratuit et sans aucune obligation !
+            <span className="text-[#71c088] font-semibold">Mana</span>. Discutons pour répondre à vos questions, explorer vos besoins, et vous aider à choisir l'offre idéale. Consultez moi, j’en serais ravi, c’est rapide, gratuit et sans aucune obligation !
           </p>
 
           {/* Bouton centré */}
