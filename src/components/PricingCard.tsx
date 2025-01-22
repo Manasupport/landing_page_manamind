b9b5462e3300c4985f29d88bd2bcfaaae7050ac2
manayanis <yanis@manadvise.fr>
2025-01-22 09:20:13 +0000

    x

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -58,7 +58,7 @@ export const PricingCard = ({
       <CardContent className="flex-grow flex flex-col">
         {children}
         
-        {shouldAddSpacing && <div className="h-28" />}
+        {shouldAddSpacing && <div className="h-24" />}
         
         <ul className="space-y-4 mb-8 flex-grow">
           {features.map((feature, index) => (
