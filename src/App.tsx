aba9c28e86b949f5d0efc1d8a5aaef70f429729b
Manasupport <support_manamind@manadvise.fr>
2025-08-31 03:10:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";  // Import Vercel Analytics
+import { Analytics } from "@vercel/analytics/react"; // Import Vercel Analytics
+
 import Index from "./pages/Index";
 import { CreateAccount } from "./components/CreateAccount";
 import { Success } from "./pages/Success";
 import AboutPage from "./pages/About";
+import Ressources from "./pages/ressources"; // <-- AJOUT
 
 const queryClient = new QueryClient();
 
@@ -22,10 +24,11 @@ const App = () => (
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/success" element={<Success />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/ressources" element={<Ressources />} /> {/* <-- AJOUT */}
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
-    <Analytics />  {/* Ajout de Vercel Analytics ici */}
+    <Analytics /> {/* Ajout de Vercel Analytics ici */}
   </QueryClientProvider>
 );
 
